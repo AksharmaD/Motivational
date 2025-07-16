160a2446c890b89530dff2d7a1fef00403d0301d
Aman Sharma <amankumarsharma9989@mail.com>
2025-07-16 14:56:52 +0530

    Setup for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/Motivational/', 
+})
